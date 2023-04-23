dddbd7881191bb0effa3e326beef51d1be1e712d
Amrahvn <amrahvn@code.edu.az>
2023-04-23 22:59:45 +0400

    addd

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Collections.Generic;
+
+  class CustomList<T> where T : class
+{
+    private T[] List;
+
+    public CustomList()
+    {
+        List = new T[0];
+    }
+
+    public void Add(T item)
+    {
+        Array.Resize (ref List, List.Length + 1);
+        List[List.Length - 1] = item;
+    }
+
+    public T find(Predicate<T> function)
+    {
+        foreach (T item in List)
+        {
+            if (function(item)) return item;
+        }
+        return null;
+    }
+}
